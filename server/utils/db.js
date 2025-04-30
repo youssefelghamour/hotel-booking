b9debb6d160626eaaca541606d2e8cf67118b89d
youssef <youssefelghamour@gmail.com>
2025-04-30 18:21:09 +0100

    Add mongodb setup: db.js

---
+++ server/utils/db.js
@@ -0,0 +1,35 @@
+/**
+ * File contains the DBClient class that manages the connection to the MongoDB database.
+ * And exports an instance of DBClient for use in other modules.
+ */
+
+const { MongoClient } = require('mongodb');
+
+
+class DBClient {
+    constructor() {
+        this.dbName = 'hotel';
+        this.url = `mongodb://localhost:27017`;
+        this.client = new MongoClient(this.url);
+        this.isAlive = false;  // Track connection status
+    }
+
+    /* Connects to MongoDB if not already connected */
+    async connect() {
+        if (!this.isAlive) {
+            try {
+                await this.client.connect();
+                this.db = this.client.db(this.dbName);
+
+                // Mark connection as established
+                this.isAlive = true;
+                console.log('db.js: Connected to MongoDB');
+            } catch (err) {
+                console.error(`db.js: Failed to connect to MongoDB: ${err.message}`);
+            }
+        }
+    }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
