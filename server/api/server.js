b42164f6b7f8f288f30dad88e16eadf96517d639
youssef <youssefelghamour@gmail.com>
2025-04-30 18:23:22 +0100

    Add API

---
+++ server/api/server.js
@@ -0,0 +1,26 @@
+/**
+ * Express API server with MongoDB connection
+ */
+const express = require('express');
+const cors = require('cors');
+const dbClient = require('../utils/db');
+
+
+const port = 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/', (req, res) => {
+    res.send('API is running!');
+});
+
+// Ensure the database connection is established before running the API app
+(async () => {
+    // Wait for the db connection
+    await dbClient.connect();
+    app.listen(port, () => {
+        console.log(`API is running on port ${port}`);
+    });
+})();
